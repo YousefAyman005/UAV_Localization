5f0d8d5a93bd1622a659d2a69edda0a0e05d19a9
elsayed <youssef.elsayed@hhi.fraunhofer.de>
2026-04-27 00:42:39 +0200

    time change

--- slurm/run_lightglue.sh
+++ slurm/run_lightglue.sh
@@ -8,7 +8,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --gpus=2
 #SBATCH --mem=32G
-#SBATCH --time=5:00
+#SBATCH --time=4:00:00
 
 METHOD=${1:-disk}
 case "$METHOD" in
@@ -33,7 +33,7 @@ apptainer run --nv \
     "${SLURM_SUBMIT_DIR}/uav_localization.sif" \
     /opt/uav_localization/lightglue_pipeline.py \
         --method "${METHOD}" \
-        --flights all \
+        --flights 01 \
         --visualize
 APPTAINER_EXIT=$?
 
